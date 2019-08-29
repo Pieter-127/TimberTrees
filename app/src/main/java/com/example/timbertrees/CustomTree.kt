d53715ea7af21e457ce105bd755f6ba652cc5aed
pieterventer <pventer@jhb.dvt.co.za>
2019-08-29 10:48:37 +0200

    ignores other logging priorities and updates layout with project description

--- app/src/main/java/com/example/timbertrees/CustomTree.kt
+++ app/src/main/java/com/example/timbertrees/CustomTree.kt
@@ -1,16 +1,18 @@
 package com.example.timbertrees
 
 import android.util.Log
-import android.util.Log.ERROR
 import timber.log.Timber
 
 class CustomTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
 
-        if (priority == ERROR) {
-            //log to Crash report service
-            Log.d("example", "This will come through as debug log: $message")
+        if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+            return
         }
+
+        //log to Crash report service
+        Log.d("example", "This will come through as debug log: $message")
+
 
     }
 }
